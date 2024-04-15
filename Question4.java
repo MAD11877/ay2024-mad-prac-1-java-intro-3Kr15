3fa0dcab7a6a97ae4ea515e1e3392eb0491bb1f3
3Kr15 <150001216+3Kr15@users.noreply.github.com>
2024-04-15 15:17:13 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,14 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter a number: ");
+    int n = in.nextInt();
+
+    for (int i = n; i >= 1 ; i--){
+      for (int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
