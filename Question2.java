9b1d7c1646f4708f81588af1fa9039b9ba5c9ecc
3Kr15 <150001216+3Kr15@users.noreply.github.com>
2024-04-15 17:42:30 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,12 +5,15 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter your height in meters:");
     double height = in.nextDouble();
+
+    System.out.println("Enter your weight in kilograms:");
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
     String formatted = String.format("%.1f", bmi);
-    System.out.println(formatted);
+    System.out.println("Your BMI is " + formatted);
     /**
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
